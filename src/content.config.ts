e7f2b46e616c78c54fb3c30c35513227e6d9f171
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2026-07-06 13:54:43 +0200

    add: texts to legal pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,36 +1,10 @@
 import { defineCollection } from 'astro:content';
 import { z } from 'zod';
-import { file } from 'astro/loaders';
 
-const localeRecord = z.object({
+const localeSchema = z.object({
   pl: z.string(),
   en: z.string(),
   de: z.string()
 });
 
-const topBoxesData = z.object({
-  availabilityBlock: z.string(),
-  infoBlock: z.string()
-});
-
-const site = defineCollection({
-  loader: file('src/content/site/config.json', {
-    parser: (text) => ({ config: JSON.parse(text) }),
-  }),
-  schema: z.object({
-    promoBar: z.object({
-      enabled: z.boolean(),
-      messages: localeRecord
-    }),
-    availability: z.object({
-      block: localeRecord
-    }),
-    topBoxes: z.object({
-      pl: topBoxesData,
-      en: topBoxesData,
-      de: topBoxesData
-    })
-  })
-});
-
-export const collections = { site };
+export const collections = {};

be78cf330a55a66d11819a3ffc51ec140cd7c1ff
erykos73 <43580519+erykos73@users.noreply.github.com>
2026-08-14 07:52:00 +0200

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -20,9 +20,9 @@ export const siteConfig: SiteConfig = {
   "topBoxes": {
     "pl": {
       "availabilityBlock": `<h2 class="promo-h2">Wolne terminy</h2>
-       <h3>">Sierpień 2026</h3>
+       <h3 class="promo-h2">">Sierpień 2026</h3>
        <p class="text-base mt-2">25 - 28 sie (D1)</p><p>25 - 30 sie (D2)</p>
-       <h3>Wrzesień 2026</h3>
+       <h3class="promo-h2">Wrzesień 2026</h3>
        <p class="text-base mt-2">8 - 25 wrz (D1)</p><p>14 - 25 wrz (D2)</p><p>28 wrz - 2 paź (D2)</p><p>28 wrz - 1 paź (D1)</p>
        
  
